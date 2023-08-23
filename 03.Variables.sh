55ef2de8f9e189cfbaf0d8b75c440b57e96e92dc
MNYeruva <mnyeruva@gmail.com>
2023-08-23 00:14:10 -0400

    JourneyOfLinux updated

---
+++ 03.Variables.sh
@@ -0,0 +1,15 @@
+# If we assign a name to a set of data is called variable.
+
+
+- In Bash shell we declare the variable as VAR=DATA
+- In Bash Shell we access the variable as $VAR OR ${VAR}
+- All the time we will not hardcode the value of a variable and we need the data dynamically
+- VAR=$(Command)
+- VAR=$((Expression))
+    - $((2+3))
+
+  name=mnyeruva
+  class=DevOps
+
+      echo trainer name - $name
+      echo class name - ${class}
