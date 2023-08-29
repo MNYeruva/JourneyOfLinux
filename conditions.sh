fc99fc5e846621bda5c3f4394308fb98ffde9489
MNYeruva <mnyeruva@gmail.com>
2023-08-29 16:28:24 -0400

    JourneyOfLinux updated

---
+++ conditions.sh
@@ -0,0 +1,19 @@
+Conditions:
+  - Case
+  - if  (3 if conditions)
+      - Simple if
+      - if Else
+      - Else if
+
+      Simple if:
+      Command-----> if -------> true----->Command
+      if Elase:
+      Command-----> if else ---> false---->Command
+                            ---->true----->Command
+
+      Else if:
+      Command----->true---->Command
+
+                    false-------> Else if ------>false------>Command
+
+                                  false---->
