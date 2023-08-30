f53352eb0e1dd60a99e49974fa051ddd1457b66f
MNYeruva <mnyeruva@gmail.com>
2023-08-29 21:13:34 -0400

    JourneyOfLinux updated

---
+++ function.sh
@@ -0,0 +1,11 @@
+# Functions are like variables
+# Variable - When we assign a name to a set of data then it is a variable.
+# Function - When we assign a name to a set of commands then is is function.
+
+Function:
+example(){
+   echo i am a example function
+}
+
+- To call a function
+     - example
