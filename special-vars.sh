39a1f03d3ddc118e4f879c79828f3c541b588c7b
MNYeruva <mnyeruva@gmail.com>
2023-08-29 22:07:28 -0400

    JourneyOfShell updated

--- special-vars.sh
+++ special-vars.sh
@@ -4,3 +4,14 @@ echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example(){
+  echo "Following values from function"
+  echo "0 - $0" # not applicable for function
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+example MN Yeruva
